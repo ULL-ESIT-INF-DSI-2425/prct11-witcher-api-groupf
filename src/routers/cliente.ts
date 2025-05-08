9fca8a7485432fe4e57dd31a5fa382a809476ffa
Adrian Leon Diaz <alu0101495668@ull.edu.es>
2025-05-08 15:00:33 +0000

    Listo2

--- src/routers/cliente.ts
+++ src/routers/cliente.ts
@@ -147,46 +147,29 @@ clienteRouter.patch('/clientes', async (req, res) => {
 });
 
 /**
- * Ruta GET para obtener clientes por tipo
- * @returns Lista de clientes del tipo especificado o un mensaje de error
+ * Ruta DELETE para eliminar un cliente por nombre usando query
+ * @returns Mensaje de éxito o error
  */
-clienteRouter.get('/clientes/tipo/:tipo', async (req, res) => {
+clienteRouter.delete('/clientes', async (req, res) => { 
   try {
-    const { tipo } = req.params;
-    const clientes = await obtenerClientesPorTipo(tipo);
+    const nombre = req.query.nombre?.toString();
 
-    if (clientes.length > 0) {
-      res.status(200).send(clientes);
+    if (!nombre || nombre.trim() === "") {
+      res.status(400).send({ mensaje: 'Se requiere un nombre válido del cliente como query (?nombre=...)' });
+      return;
+    }
+    const clientes = await obtenerClientePorNombre(nombre);
+    if (clientes.length === 0) {
+      res.status(404).send({ mensaje: `Cliente con nombre "${nombre}" no encontrado.` });
+      return;
+    }
+    const clienteEliminado = await eliminarCliente(clientes[0]._id as string);
+    if (clienteEliminado) {
+      res.status(200).send({ mensaje: `Cliente con nombre "${nombre}" eliminado correctamente.` });
     } else {
-      res.status(404).send({ 
-        mensaje: `No se encontraron clientes del tipo '${tipo}'.`,
-        sugerencia: 'Los tipos válidos son: Cazador, Brujo, Noble, Bandido, Mercenario, Aldeano'
-      });
+      res.status(404).send({ mensaje: `Error al eliminar el cliente con nombre "${nombre}".` });
     }
   } catch (error) {
-    res.status(500).send({ 
-      mensaje: 'Error al buscar clientes por tipo', 
-      error: error instanceof Error ? error.message : error 
-    });
-  }
-});
-
-/**
- * Ruta GET para obtener clientes por dinero
- * @returns Lista de clientes con una cantidad exacta de dinero o un mensaje de error
- */
-clienteRouter.get('/clientes/dinero/:dinero', async (req, res) => {
-  try {
-    const { dinero } = req.params;
-    const dineroNumerico = parseFloat(dinero);
-    const clientes = await obtenerClientesPorDinero(dineroNumerico);
-
-    if (clientes.length > 0) {
-      res.status(200).send(clientes);
-    } else {
-      res.status(404).send({ mensaje: 'No se encontraron clientes con esa cantidad exacta de dinero.' });
-    }
-  } catch (error) {
-    res.status(500).send({ mensaje: 'Error al buscar clientes por dinero', error });
+    res.status(500).send({ mensaje: 'Error al eliminar el cliente.', error: error instanceof Error ? error.message : error });
   }
 });
